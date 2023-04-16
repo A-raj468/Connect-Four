9aee16c8fdaf72ee44a9c37ca13cf61ebbf65c39
Aditya <adityraj6508@gmail.com>
2023-04-16 14:37:44 +0530

    Made the script more readable

--- new_script.js
+++ new_script.js
@@ -1,13 +1,82 @@
 /**@type {HTMLTableElement}*/
-var table = document.getElementById('game');
+let board = document.getElementById('game');
 
 const M = 6;
 const N = 7;
 
-var tiles = new Array(M);
+let tiles = new Array(M);
 
-var turn = 1;
-var empty = N*M;
+let turn = 1;
+let empty = N*M;
+
+/**
+ * @param {HTMLTableCellElement[][]} tiles
+ * @param {number} x
+ * @param {number} y
+ */
+checkWin = function(tiles, x, y){
+    // row checking
+    for(let i=0; i<4; i++){
+        let total = 0;
+        for(let j=0; j<4; j++){
+            let r = x;
+            let c = y+i-j;
+            if((c<0 || c>=N) || (r<0 || r>=M)) break;
+            total += tiles[r][c].occupied;
+        }
+        if(total == 4 || total == -4){
+            console.log("Row win!");
+            return tiles[x][y].occupied;
+        }
+    }
+
+    // col checking
+    for(let i=0; i<4; i++){
+        let total = 0;
+        for(let j=0; j<4; j++){
+            let r = x+i-j;
+            let c = y;
+            if((c<0 || c>=N) || (r<0 || r>=M)) break;
+            total += tiles[r][c].occupied;
+        }
+        if(total == 4 || total == -4){
+            console.log("Col win!");
+            return tiles[x][y].occupied;
+        }
+    }
+
+    // primDiag checking
+    for(let i=0; i<4; i++){
+        let total = 0;
+        for(let j=0; j<4; j++){
+            let r = x+i-j;
+            let c = y+i-j;
+            if((c<0 || c>=N) || (r<0 || r>=M)) break;
+            total += tiles[r][c].occupied;
+        }
+        if(total == 4 || total == -4){
+            console.log("PriDiag win!");
+            return tiles[x][y].occupied;
+        }
+    }
+    
+    // secDiag checking
+    for(let i=0; i<4; i++){
+        let total = 0;
+        for(let j=0; j<4; j++){
+            let r = x+i-j;
+            let c = y-i+j;
+            if((c<0 || c>=N) || (r<0 || r>=M)) break;
+            total += tiles[r][c].occupied;
+        }
+        if(total == 4 || total == -4){
+            console.log("SecDiag win!");
+            return tiles[x][y].occupied;
+        }
+    }
+
+    return 0;
+}
 
 /**
  * @param {HTMLTableElement} table
@@ -18,14 +87,15 @@ var empty = N*M;
 board_initialize = function(table, M, N, tiles){
     for(let i=0; i<M; i++){
         tiles[i] = new Array(N);
-        var tr = table.insertRow();
+        let tr = table.insertRow();
         for(let j=0; j<N; j++){
-            var td = tr.insertCell();
-            var img = document.createElement('img');
+            let td = tr.insertCell();
+            let img = document.createElement('img');
+            img.draggable = false;
             td.occupied = 0;
             td.appendChild(img);
             td.draw = function(){
-                var image = this.querySelector('img');
+                let image = this.querySelector('img');
                 if(this.occupied == -1){
                     image.src = "Img/yellow.png";
                 }
@@ -36,10 +106,36 @@ board_initialize = function(table, M, N, tiles){
                     image.src = "Img/red.png";
                 }
             }
+            td.addEventListener('click', function(event){
+                event.preventDefault();
+                let col = this.cellIndex;
+                for(let r=M; r>0; r--){
+                    if(turn != 0){
+                        let currtd = tiles[r-1][col];
+                        if(currtd.occupied == 0){
+                            currtd.occupied = turn;
+                            currtd.draw();
+                            turn = -turn;
+                            let w = checkWin(tiles, r-1, col);
+                            if(w != 0){
+                                turn = 0;
+                                let msz = "Player " + (w == 1? "1":"2") + " wins!";
+                                console.log(msz);
+                                document.querySelector('#msz').innerHTML = msz;
+                                toggle_popup();
+                            }
+                            break;
+                        }
+                    }
+                }
+            });
+            td.addEventListener('drag', function(event){event.preventDefault();});
             td.draw();
             tiles[i][j] = td;
         }
     }
 }
 
-board_initialize(table, M, N, tiles);
+toggle_popup = function(){
+    document.getElementById('popup').classList.toggle('active');
+}
